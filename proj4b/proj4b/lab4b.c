1be164db1cfcc158f2cbfa56348afdccb50aa4b6
Stewart Dulaney <sdulaney@users.noreply.github.com>
2020-03-01 06:55:17 -0800

    Update files

--- proj4b/proj4b/lab4b.c
+++ proj4b/proj4b/lab4b.c
@@ -11,19 +11,43 @@
 #include <errno.h>
 #include <mraa.h>
 #include <signal.h>
+#include <math.h>
 
-//int period = 1;
-
+int period = 1;
 sig_atomic_t volatile run_flag = 1;
+mraa_aio_context temp_sensor;
+mraa_gpio_context button;
 
 void do_when_interrupted() {
   run_flag = 0;
 }
 
-int main() {
+float get_temp(char* scale) {
+    if (scale == NULL)
+	return 0.0;
+    int input_voltage = mraa_aio_read(temp_sensor);
+    if (input_voltage == -1) {
+	fprintf(stderr, "Error reading from AIO 1 (temperature sensor).\n");
+	mraa_deinit();
+	exit(1);
+    }
+    const int B = 4275;
+    const int R0 = 100000;
+    float R = 1023.0/input_voltage-1.0;
+    R = R0*R;
+    float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15;
+    if (*scale == 'C') {
+	return temperature;
+    }
+    else {
+	return temperature * (9/5) + 32;
+    }
+}
+
+int main(int argc, char ** argv) {
 
     // Process all arguments
-  /*  int c;
+    int c;
     int opt_period = 0;
     int opt_scale = 0;
     int opt_log = 0;
@@ -94,27 +118,59 @@ int main() {
             printf("?? getopt returned character code 0%o ??\n", c);
         }
     }
-*/
 
-    mraa_gpio_context buzzer, button;
-    buzzer = mraa_gpio_init(62);
+    // Initialize AIO pin for temperature sensor
+    temp_sensor = mraa_aio_init(1);
+    if (temp_sensor == NULL) {
+	fprintf(stderr, "Error initializing AIO 1 (temperature sensor).\n");
+	mraa_deinit();
+	exit(1);
+    }
+
+    // Initialize GPIO pin for the button
     button = mraa_gpio_init(60);
+    if (button == NULL) {
+	fprintf(stderr, "Error initializing GPIO 60 (button).\n");
+	mraa_deinit();
+	exit(1);
+    }
 
-    mraa_gpio_dir(buzzer, MRAA_GPIO_OUT);
-    mraa_gpio_dir(button, MRAA_GPIO_IN);
+    // Use for MRAA error checking
+    int status;
 
+    // Set GPIO button to input
+    status = mraa_gpio_dir(button, MRAA_GPIO_IN);
+    if (status != MRAA_SUCCESS) {
+	fprintf(stderr, "Error setting GPIO 60 (button) to input.\n");
+	mraa_deinit();
+	exit(1);
+    }
+
+    // When the button is pressed, call do_when_interrupted
     mraa_gpio_isr(button, MRAA_GPIO_EDGE_RISING, &do_when_interrupted, NULL);
 
     while (run_flag) {
-	mraa_gpio_write(buzzer, 1);
-	sleep(1);
-	mraa_gpio_write(buzzer, 0);
+	float temperature = get_temp(arg_scale);
+	fprintf(stdout, "%f\n", temperature);
 	sleep(1);
     }
 
-    mraa_gpio_write(buzzer, 0);
-    mraa_gpio_close(buzzer);
-    mraa_gpio_close(button);
+    // Close AIO temperature sensor
+    status = mraa_aio_close(temp_sensor);
+    if (status != MRAA_SUCCESS) {
+	fprintf(stderr, "Error closing AIO 1 (temperature sensor).\n");
+	mraa_deinit();
+	exit(1);
+    }
+
+    // Close GPIO button
+    status = mraa_gpio_close(button);
+    if (status != MRAA_SUCCESS) {
+	fprintf(stderr, "Error closing GPIO 60 (button).\n");
+	mraa_deinit();
+	exit(1);
+    }
+    
   
     exit(0);
 }
